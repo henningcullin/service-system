0be856498f78dc1b81caf723cfcff4ef8c23ab53
Henning Cullin <henningcullin@gmail.com>
2024-02-15 18:34:02 +0100

    feat: details function added

--- backend/src/facility.rs
+++ backend/src/facility.rs
@@ -1,10 +1,36 @@
 use axum::{
-    extract::Json,
+    extract::{
+        State,
+        Json
+    },
     http::StatusCode
 };
+use sqlx::{
+    MySqlPool,
+    FromRow,
+};
+use serde_derive::{
+    Serialize,
+    Deserialize
+};
 
-use serde_derive::Deserialize;
+#[derive(Serialize, Deserialize, FromRow)]
+pub struct Facility {
+    facility_id: i32,
+    name: String
+}
 
 pub async fn details() {
     
+}
+
+pub async fn index(
+    State(pool): State<MySqlPool>
+) -> Result<Json<Vec<Facility>>, StatusCode> {
+    let facilities = sqlx::query_as::<_, Facility>("SELECT * FROM facility")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    return Ok(Json(facilities));
 }
