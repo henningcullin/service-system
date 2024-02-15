e80da33a7a155b7f88067c88a6f68dd4764efafd
Henning Cullin <henningcullin@gmail.com>
2024-02-16 00:00:58 +0100

    fix: type to machine_type

--- structure/tables/machine.sql
+++ structure/tables/machine.sql
@@ -1,6 +1,6 @@
 CREATE TABLE machine (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    type VARCHAR(255),
+    machine_type VARCHAR(255),
     status ENUM('Active', 'Inactive') DEFAULT 'Inactive'
 );
