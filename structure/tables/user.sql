7dfdfd7d294baa5ff9abd305a360ac3bdf63b96e
Henning Cullin <henningcullin@gmail.com>
2024-02-18 22:28:08 +0100

    feat: ready database

--- structure/tables/user.sql
+++ structure/tables/user.sql
@@ -1,8 +1,13 @@
 CREATE TABLE user (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    phone INT UNSIGNED UNIQUE,
-    role ENUM('Super', 'Administrator', 'Basic') DEFAULT 'Basic'
+    password VARCHAR(255),
+    phone VARCHAR(255) UNIQUE,
+    role ENUM('Super', 'Administrator', 'Basic', 'Worker') NOT NULL DEFAULT 'Worker',
+    last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT check_password_for_non_worker CHECK (NOT (role != 'Worker' AND password IS NULL))
 );
+
+CREATE INDEX idx_email ON user (email);
--- structure/tables/worker.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE worker (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
-    phone INT UNSIGNED,
-    company VARCHAR(255) NOT NULL
-);
