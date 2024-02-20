45d83ba8efb1b5603f598259844e6de92dad5da7
Henning Cullin <henningcullin@gmail.com>
2024-02-20 14:59:32 +0100

    feat: authentication middleware

---
+++ backend/src/auth.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+
+use axum::{
+    body::Body, extract::State, http::{header, Request, StatusCode}, middleware::Next, response::IntoResponse, Json
+};
+
+use axum_extra::extract::cookie::CookieJar;
+use jsonwebtoken::{decode, DecodingKey, Validation};
+use serde::Serialize;
+
+use crate::{
+    user::{TokenClaims, User},
+    AppState,
+};
+
+#[derive(Debug, Serialize)]
+pub struct ErrorResponse {
+    pub status: &'static str,
+    pub message: String,
+}
+
+pub async fn auth(
+    cookie_jar: CookieJar,
+    State(app_state): State<Arc<AppState>>,
+    mut req: Request<Body>,
+    next: Next,
+) -> Result<impl IntoResponse, (StatusCode, Json<ErrorResponse>)> {
+    let token = cookie_jar
+        .get("token")
+        .map(|cookie| cookie.value().to_string())
+        .or_else(|| {
+            req.headers()
+                .get(header::AUTHORIZATION)
+                .and_then(|auth_header| auth_header.to_str().ok())
+                .and_then(|auth_value| {
+                    if auth_value.starts_with("Bearer ") {
+                        Some(auth_value[7..].to_owned())
+                    } else {
+                        None
+                    }
+                })
+        });
+
+    let token = token.ok_or_else(|| {
+        let json_error = ErrorResponse {
+            status: "Fail",
+            message: "You are not logged in, please provide token".to_string(),
+        };
+        (StatusCode::UNAUTHORIZED, Json(json_error))
+    })?;
+
+    let claims = decode::<TokenClaims>(
+        &token,
+        &DecodingKey::from_secret(app_state.env.jwt_secret.as_ref()),
+        &Validation::default(),
+    )
+    .map_err(|_| {
+        let json_error = ErrorResponse {
+            status: "fail",
+            message: "Invalid token".to_string(),
+        };
+        (StatusCode::UNAUTHORIZED, Json(json_error))
+    })?
+    .claims;
+
+    let user_id = uuid::Uuid::parse_str(&claims.sub).map_err(|_| {
+        let json_error = ErrorResponse {
+            status: "fail",
+            message: "Invalid token".to_string(),
+        };
+        (StatusCode::UNAUTHORIZED, Json(json_error))
+    })?;
+
+    let user = sqlx::query_as::<_, User>("SELECT id, first_name, last_name, email, password, phone, CAST(role AS SIGNED) role, last_login FROM user WHERE id = ?")
+        .bind(user_id)
+        .fetch_optional(&app_state.db)
+        .await
+        .map_err(|e| {
+            let json_error = ErrorResponse {
+                status: "fail",
+                message: format!("Error fetching user from databasebase: {}", e),
+            };
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(json_error))
+        })?;
+
+    let user = user.ok_or_else(|| {
+        let json_error = ErrorResponse {
+            status: "fail",
+            message: "The user belonging to this token no longer exists".to_string(),
+        };
+        (StatusCode::UNAUTHORIZED, Json(json_error))
+    })?;
+
+    req.extensions_mut().insert(user);
+    Ok(next.run(req).await)
+}
