90359b8906664be1f78d6501e598e09bfb4987fa
Henning Cullin <henningcullin@gmail.com>
2024-02-26 15:38:14 +0100

    fix: Super user pwl

--- structure/tables/user.sql
+++ structure/tables/user.sql
@@ -3,12 +3,12 @@ CREATE TABLE user (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255),
+    password VARCHAR(255) NULL,
     phone VARCHAR(255) UNIQUE,
     role ENUM('Super', 'Administrator', 'Basic', 'Worker') NOT NULL DEFAULT 'Worker',
     active BOOLEAN NOT NULL DEFAULT TRUE,
     last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT check_password_for_non_worker CHECK (NOT (role != 'Worker' AND password IS NULL))
+    CHECK (password IS NULL OR (role NOT IN ('Worker', 'Super') AND password IS NOT NULL) OR (role IN ('Worker', 'Super') AND password IS NULL))
 );
 
 CREATE INDEX idx_email ON user (email);
