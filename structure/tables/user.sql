845d4cea9dcfa32dbf0a30e0c1e13874af393dc2
Henning Cullin <henningcullin@gmail.com>
2024-02-27 13:59:23 +0100

    feat: add super user

--- structure/tables/user.sql
+++ structure/tables/user.sql
@@ -12,3 +12,6 @@ CREATE TABLE user (
 );
 
 CREATE INDEX idx_email ON user (email);
+
+INSERT INTO user (first_name, last_name, email, phone, role)
+VALUES ('Axami', 'Systems', 'henade0401@edu.halmstad.se', '0761999773', 'Super');
