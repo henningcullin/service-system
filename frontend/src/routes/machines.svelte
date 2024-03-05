9d1d1adcebd6b2ee03d4e2496d2cb9883ae1bd47
Henning Cullin <henningcullin@gmail.com>
2024-03-05 14:18:04 +0100

    feat: convert to javascript dates

--- frontend/src/routes/machines.svelte
+++ frontend/src/routes/machines.svelte
@@ -7,6 +7,13 @@
         try {
             const response = await fetch('/api/auth/machines');
             const data = await response.json();
+
+            const formatted = data.map((/** @type {{ created: string | number | Date; edited: string | number | Date; }} */ machine) => {
+                machine.created = new Date(machine.created);
+                machine.edited = new Date(machine.edited);
+                return machine;
+            })
+
             machines.set(data);
         } catch (error) {
             console.log('Could not fetch products', error);
@@ -15,17 +22,39 @@
 
 </script>
 
-<h2> Welcome to the Machine page!!</h2>
-
-<div class="machine-grid">
-    {#each $machines as machine}
-        <div> 
-            <b>Name: {machine.name}</b>
-            <p>Make: {machine.make}</p>
-            <p>Type: {machine.machine_type}</p>
-            <p>Status: {machine.status}</p>
-            <i>Created: {machine.created}</i><br>
-            <i>Edited: {machine.edited}</i>
-        </div>
-    {/each}
+<div class="segment">
+    <h2> Welcome to the Machine page!!</h2>
+    
+    <div class="machine-grid">
+        {#each $machines as machine}
+            <div class="machine-card"> 
+                <b>Name: {machine.name}</b>
+                <p>Make: {machine.make}</p>
+                <p>Type: {machine.machine_type}</p>
+                <p>Status: {machine.status}</p>
+                <i title={machine.created.toLocaleTimeString()}>Created: {machine.created.toLocaleDateString()}</i><br>
+                <i title={machine.edited.toLocaleTimeString()}>Edited: {machine.edited.toLocaleDateString()}</i>
+            </div>
+        {/each}
+    </div>
 </div>
+
+
+<style>
+    .machine-grid {
+        margin-top: 50px;
+        display:grid;
+        width:100%;
+        grid-template-columns: 1fr, 1fr;
+        gap:10px;
+        padding:0.5%;
+    }
+
+    .machine-card {
+        padding: 0.5%;
+        width:100%;
+        background-color: #353535;
+        border-radius: 5px;
+    }
+
+</style>
