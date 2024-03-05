d2e3be0f9ddf14965175e468eb48aaee2d12c19c
Henning Cullin <henningcullin@gmail.com>
2024-03-05 10:29:18 +0100

    feat: fetch machines from api

--- frontend/src/routes/machines.svelte
+++ frontend/src/routes/machines.svelte
@@ -1,8 +1,31 @@
 <script>
 
-    console.log("We are now in the Machine page!");
+    import { onMount } from 'svelte';
+    import { machines } from '../lib/stores.js'
+
+    onMount(async () => {
+        try {
+            const response = await fetch('/api/auth/machines');
+            const data = await response.json();
+            machines.set(data);
+        } catch (error) {
+            console.log('Could not fetch products', error);
+        }
+    });
 
 </script>
 
-
 <h2> Welcome to the Machine page!!</h2>
+
+<div class="machine-grid">
+    {#each $machines as machine}
+        <div> 
+            <b>Name: {machine.name}</b>
+            <p>Make: {machine.make}</p>
+            <p>Type: {machine.machine_type}</p>
+            <p>Status: {machine.status}</p>
+            <i>Created: {machine.created}</i><br>
+            <i>Edited: {machine.edited}</i>
+        </div>
+    {/each}
+</div>
