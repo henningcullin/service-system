0385427ff999fb6820a3d6caa36fd6caf73656ac
Henning Cullin <henningcullin@gmail.com>
2024-03-09 19:06:21 +0100

    feat: display machine

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const machines = writable([]);
-export const machine = writable([]);
+export const machine = writable({});
 export const account = writable({});
--- frontend/src/routes/machines/machine.svelte
+++ frontend/src/routes/machines/machine.svelte
@@ -1,4 +1,5 @@
 <script>
+    // @ts-nocheck
     import { machine } from '../../lib/stores';
     import { navigate } from 'svelte-navigator';
 
@@ -7,22 +8,21 @@
 
     if (!id || id.length != 36) navigate('/notfound');
 
-    machine.set([]);
-
     async function getMachine() {
         try {
             const response = await fetch('/api/auth/machine?id='+ id);
             const data = await response.json();
 
             // @ts-ignore
-            machine.set([{id: data.id,
+            machine.set({
+                id: data.id,
                 name: data.name,
                 make: data.make,
                 type: data.machine_type,
                 status: data.status,
-                created: new Date(data.created),
-                edited: new Date(data.edited),
-            }]);
+                created: new Date(data.created).toLocaleString('en-GB'),
+                edited: new Date(data.edited).toLocaleString('en-GB'),
+            });
 
         } catch (error) {
             console.error(error)
@@ -31,17 +31,48 @@
 
     getMachine();
 
+    $: m = $machine;
+
 </script>
 
 <div class='segment'>
 
-    {#each $machine as machine}
-        <b>Name: {machine.name}</b>
-        <p>Make: {machine.make}</p>
-        <p>Type: {machine.type}</p>
-        <p>Status: {machine.status}</p>
-        <i title={machine.created.toLocaleTimeString()}>Created: {machine.created.toLocaleDateString()}</i><br>
-        <i title={machine.edited.toLocaleTimeString()}>Edited: {machine.edited.toLocaleDateString()}</i>
-    {/each}
+    <form>
+        <label for='id'>ID</label>
+        <input id='id' type='text' value='{m.id}' disabled readonly>
+
+        <label for='name'>Name</label>
+        <input id='name' type='text' bind:value={m.name} readonly>
+        
+        <label for='make'>Make</label>
+        <input id='make' type='text' bind:value={m.make} readonly>
+        
+        <label for='type'>Type</label>
+        <input id='type' type='text' bind:value={m.type} readonly>
+        
+        <label for='status'>Status</label>
+        <input id='status' type='text' bind:value={m.status} readonly>
+        
+        <label for='created'>Created</label>
+        <input id='created' type='text' value='{m.created}' disabled readonly>
+        
+        <label for='edited'>Edited</label>
+        <input id='edited' type='text' value='{m.edited}' disabled readonly>
+    </form>
 
 </div>
+
+<style>
+
+    label {
+        font-size:1.1em;
+        text-align: left;
+    }
+
+    input {
+        margin-bottom: 25px;
+        font-size:1.15em;
+        line-height: 1.5em;
+    }
+
+</style>
