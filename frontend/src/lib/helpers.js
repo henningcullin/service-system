509988be42b2d20838999c63fd876b246e97de40
Henning Cullin <henningcullin@gmail.com>
2024-03-19 21:17:30 +0100

    fix: change getUser to getLoggedIn

--- frontend/src/lib/helpers.js
+++ frontend/src/lib/helpers.js
@@ -5,22 +5,20 @@ import { navigate } from 'svelte-navigator';
  * Sets the account writable to the currently logged in user
  * @returns {Promise<void>}
  */
-export async function getUser() {
-  try {
-    const response = await fetch('/api/auth/user/me');
-    const data = await response.json();
+export async function getLoggedIn() {
+	try {
+		const response = await fetch('/api/auth/user/me');
+		const data = await response.json();
 
-    if (response.status !== 200) return navigate('/login');
+		if (response.status !== 200) return navigate('/login');
 
-    // @ts-ignore
-    data.last_login = new Date(data.last_login);
+		data.last_login = new Date(data.last_login);
 
-    account.set(data);
-  } catch (error) {
-    console.log('Could not fetch products', error);
-  }
+		account.set(data);
+	} catch (error) {
+		console.log('Could not fetch products', error);
+	}
 }
-
 
 /**
  * Sends json data
@@ -30,13 +28,13 @@ export async function getUser() {
  * @returns {Promise<any>}
  */
 export async function sendJson(url, method, body) {
-  return fetch(url, {
-    headers: {
-      'Content-Type':'application/json'
-    },
-    method: method,
-    body: JSON.stringify(body)
-  });
+	return fetch(url, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: method,
+		body: JSON.stringify(body),
+	});
 }
 
 /**
@@ -45,14 +43,14 @@ export async function sendJson(url, method, body) {
  * @returns {Promise<any>}
  */
 export async function sendDelete(url) {
-  return fetch(url, { method: 'DELETE'})
+	return fetch(url, { method: 'DELETE' });
 }
 
 /**
  * Shorthand for document.querySelector
- * @param {string} selector 
+ * @param {string} selector
  * @returns {Element | null}
  */
 export function el(selector) {
-  return document.querySelector(selector);
+	return document.querySelector(selector);
 }
