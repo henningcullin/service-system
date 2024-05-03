607b8ff56b1841d73af4a5774af8549e73593a8d
Henning Cullin <henningcullin@gmail.com>
2024-05-03 15:03:46 +0200

    feat: facility details

--- backend/src/machines/facilities/handlers.rs
+++ backend/src/machines/facilities/handlers.rs
@@ -1,22 +1,51 @@
 use std::sync::Arc;
 
-use axum::{extract::State, Extension, Json};
+use axum::{
+    extract::{Query, State},
+    Extension, Json,
+};
 use sqlx::query_as;
 
 use crate::{
     machines::facilities::Facility,
     users::models::User,
-    utils::errors::{ApiError, ForbiddenReason},
+    utils::{check_permission, errors::ApiError},
     AppState,
 };
+
+use super::models::QueryFacility;
+
+pub async fn details(
+    Extension(user): Extension<User>,
+    State(app_state): State<Arc<AppState>>,
+    Query(params): Query<QueryFacility>,
+) -> Result<Json<Facility>, ApiError> {
+    check_permission(user.role.facility_view)?;
+
+    let facility = query_as!(
+        Facility,
+        r#"
+        SELECT
+            *
+        FROM
+            facilities f
+        WHERE
+            f.id = $1
+        "#,
+        params.id
+    )
+    .fetch_one(&app_state.db)
+    .await
+    .map_err(ApiError::from)?;
+
+    Ok(Json(facility))
+}
 
 pub async fn index(
     Extension(user): Extension<User>,
     State(app_state): State<Arc<AppState>>,
 ) -> Result<Json<Vec<Facility>>, ApiError> {
-    if !user.role.facility_view {
-        return Err(ApiError::Forbidden(ForbiddenReason::MissingPermission));
-    }
+    check_permission(user.role.facility_view)?;
 
     let facilities = query_as!(
         Facility,
