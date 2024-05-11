526939758e67f3915252c02c3f91794c061193a2
Henning Cullin <henningcullin@gmail.com>
2024-05-11 19:49:19 +0200

    ci: stores and utils aliases

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     resolve: {
         alias: {
             $components: path.resolve('src/lib/components'),
+            $stores: path.resolve('src/lib/stores.js'),
+            $utils: path.resolve('src/lib/utils.js'),
             $lib: path.resolve('src/lib'),
             $routes: path.resolve('src/routes'),
         },
