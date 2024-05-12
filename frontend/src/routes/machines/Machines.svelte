1b756b6054edc87fb458c34f5e21be0e2218082a
Henning Cullin <henningcullin@gmail.com>
2024-05-12 21:29:48 +0200

    feat: show all machines

--- frontend/src/routes/machines/Machines.svelte
+++ frontend/src/routes/machines/Machines.svelte
@@ -6,29 +6,63 @@
     getMachines();
 </script>
 
-<list>
+<container>
     {#each $machines as machine}
         <card>
-            <Link to="/machine/{machine.id}"
-                >{#if machine.make}
-                    {machine.make + ','}
-                {/if}
-                {machine.name}</Link
-            >
-            <Link to="/machine/type/{machine.machine_type.id}">{machine.machine_type.name}</Link>
-            <Link to="/machine/status/{machine.status.id}">{machine.status.name}</Link>
+            <div class="header">
+                <Link to="/machine/{machine.id}"
+                    >{#if machine.make}
+                        {machine.make + ','}
+                    {/if}
+                    {machine.name}</Link
+                >
+            </div>
+            <div>
+                <b>Type</b>
+                <Link to="/machine/type/{machine.machine_type.id}">{machine.machine_type.name}</Link>
+            </div>
+            <div>
+                <b>Status</b>
+                <Link to="/machine/status/{machine.status.id}">{machine.status.name}</Link>
+            </div>
+            <div>
+                <b>Created</b>
+                {new Date(machine.created).toLocaleString()}
+            </div>
+            <div>
+                <b>Edited</b>
+                {new Date(machine.edited).toLocaleString()}
+            </div>
             {#if machine.facility.id}
-                <Link to="/facility/{machine.facility.id}">{machine.facility.name}</Link>
+                <hr />
+                <div>
+                    <b>Facility</b>
+                    <Link to="/facility/{machine.facility.id}">{machine.facility.name}</Link>
+                    {#if machine.facility.address}
+                        <div>
+                            <b>Address</b>
+                            {machine.facility.address}
+                        </div>
+                    {/if}
+                </div>
             {/if}
         </card>
     {/each}
-</list>
+</container>
 
 <style>
-    list {
+    container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        place-items: center;
+        padding: 2%;
+        gap: 15px;
     }
 
     card {
-        padding: 10%;
+        padding: 2%;
+        background-color: var(--secondary);
+        min-height: 80%;
+        width: 80%;
     }
 </style>
