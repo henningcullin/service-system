093b1aa67ea013a038ff1961df676dbc67bc45b7
Henning Cullin <henningcullin@gmail.com>
2024-05-12 21:17:30 +0200

    feat: basic printing of machines

--- frontend/src/routes/machines/Machines.svelte
+++ frontend/src/routes/machines/Machines.svelte
@@ -1,2 +1,34 @@
 <script>
+    import { machines } from '$stores';
+    import { getMachines } from '$utils';
+    import { Link } from 'svelte-navigator';
+
+    getMachines();
 </script>
+
+<list>
+    {#each $machines as machine}
+        <card>
+            <Link to="/machine/{machine.id}"
+                >{#if machine.make}
+                    {machine.make + ','}
+                {/if}
+                {machine.name}</Link
+            >
+            <Link to="/machine/type/{machine.machine_type.id}">{machine.machine_type.name}</Link>
+            <Link to="/machine/status/{machine.status.id}">{machine.status.name}</Link>
+            {#if machine.facility.id}
+                <Link to="/facility/{machine.facility.id}">{machine.facility.name}</Link>
+            {/if}
+        </card>
+    {/each}
+</list>
+
+<style>
+    list {
+    }
+
+    card {
+        padding: 10%;
+    }
+</style>
