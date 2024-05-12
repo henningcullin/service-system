c7059059d0bd9e50fae3c106dad712e2a754a729
Henning Cullin <henningcullin@gmail.com>
2024-05-12 20:52:04 +0200

    feat: get logged in user on page load

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,28 @@
 <script>
     import Header from '$components/Header.svelte';
     import Sidebar from '$components/Sidebar.svelte';
-    import { Router, Route } from 'svelte-navigator';
+    import { Router, Route, navigate } from 'svelte-navigator';
 
-    import { SidebarOpen } from '$lib/stores';
+    import { SidebarOpen, account } from '$lib/stores';
     import Login from '$routes/Login.svelte';
     import Mainmenu from '$routes/Mainmenu.svelte';
     import NotFound from '$routes/NotFound.svelte';
+    import Account from '$routes/Account.svelte';
+    import { getLoggedIn } from '$utils';
+    import { onMount } from 'svelte';
 
     $: IsSidebarOpen = $SidebarOpen;
 
     function closeSidebar() {
         if (IsSidebarOpen) SidebarOpen.update((state) => !state);
     }
+
+    onMount(async function () {
+        if (!$account.id) {
+            const successState = await getLoggedIn();
+            if (!successState) navigate('/login');
+        }
+    });
 </script>
 
 <Router primary={false}>
@@ -22,6 +32,7 @@
         <main>
             <Route path="/" component={Mainmenu} />
             <Route path="/login/" component={Login} />
+            <Route path="/account/" component={Account} />
             <Route path="*" component={NotFound} />
         </main>
     </pusher>
