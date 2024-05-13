d986ba5fe75e50016f01c767cbb76e9d8232e90a
Henning Cullin <henningcullin@gmail.com>
2024-05-13 08:54:05 +0200

    feat: added all stores

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -6,7 +6,36 @@ export const account = writable({});
 
 export const machines = writable([]);
 export const machine = writable({});
+
 export const machineTypes = writable([]);
+export const machineType = writable({});
+
 export const machineStatuses = writable([]);
+export const machineStatus = writable({});
 
 export const facilities = writable([]);
+export const facility = writable({});
+
+export const users = writable([]);
+export const user = writable({});
+
+export const roles = writable([]);
+export const role = writable({});
+
+export const reports = writable([]);
+export const report = writable({});
+
+export const reportTypes = writable([]);
+export const reportType = writable({});
+
+export const reportStatuses = writable([]);
+export const reportStatus = writable({});
+
+export const tasks = writable([]);
+export const task = writable({});
+
+export const taskTypes = writable([]);
+export const taskType = writable({});
+
+export const taskStauses = writable([]);
+export const taskStatus = writable({});
