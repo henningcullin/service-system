3663379677854005efc32a6751ebff9291e776f2
Henning Cullin <henningcullin@gmail.com>
2024-05-13 18:11:25 +0200

    feat: better handling of selects

--- frontend/src/routes/machines/Machine.svelte
+++ frontend/src/routes/machines/Machine.svelte
@@ -1,7 +1,16 @@
 <script>
-    import { facilities, machine, machineStatuses, machineTypes } from '$stores';
+    import { facilities, machineStatuses, machineTypes } from '$stores';
     import { getFacilities, getMachine, getMachineStatuses, getMachineTypes } from '$utils';
     import { onMount } from 'svelte';
+
+    const form = {
+        id: '',
+        name: '',
+        make: '',
+        machine_type: '',
+        status: '',
+        facility: '',
+    };
 
     getMachineTypes();
     getMachineStatuses();
@@ -35,30 +44,33 @@
 
     <form>
         <label for="id">ID</label>
-        <input type="text" id="id" bind:value={$machine.id} disabled />
+        <input type="text" id="id" bind:value={form.id} disabled />
 
         <label for="name">Name</label>
-        <input type="text" id="name" bind:value={$machine.name} placeholder="Name" disabled={isViewing} />
+        <input type="text" id="name" bind:value={form.name} placeholder="Name" disabled={isViewing} />
 
         <label for="make">Make</label>
-        <input type="text" id="make" bind:value={$machine.make} placeholder="Make" disabled={isViewing} />
+        <input type="text" id="make" bind:value={form.make} placeholder="Make" disabled={isViewing} />
 
         <label for="type">Type</label>
-        <select id="type" value="hi" disabled={isViewing}>
+        <select id="type" disabled={isViewing} bind:value={form.machine_type}>
+            <option value="" class="hidden" disabled selected></option>
             {#each $machineTypes as machineType}
                 <option value={machineType.id}>{machineType.name}</option>
             {/each}
         </select>
 
         <label for="status">Status</label>
-        <select id="status" disabled={isViewing}>
+        <select id="status" disabled={isViewing} bind:value={form.status}>
+            <option value="" class="hidden" disabled selected placeholder></option>
             {#each $machineStatuses as machineStatus}
                 <option value={machineStatus.id}>{machineStatus.name}</option>
             {/each}
         </select>
 
         <label for="facility">Facility</label>
-        <select id="facility" disabled={isViewing}>
+        <select id="facility" disabled={isViewing} bind:value={form.facility}>
+            <option value="" class="hidden" disabled selected placeholder></option>
             {#each $facilities as facility}
                 <option value={facility.id}>{facility.name}</option>
             {/each}
@@ -69,6 +81,3 @@
 <tab> </tab>
 
 <tab> </tab>
-
-<style>
-</style>
