f57b36f1d1629deb5f3b2d4bda7377cc620c67db
Henning Cullin <henningcullin@gmail.com>
2024-05-13 11:48:16 +0200

    feat: action container

--- frontend/src/routes/machines/Machine.svelte
+++ frontend/src/routes/machines/Machine.svelte
@@ -18,34 +18,49 @@
     $: isViewing = !(isCreating || isEditing);
 </script>
 
-<form>
-    <label for="id">ID</label>
-    <input type="text" id="id" bind:value={$machine.id} disabled />
+<tab>
+    <div class="action container">
+        <button on:click={newMachine}>New</button>
+        <button on:click={editMachine}>Edit</button>
+        <button on:click={deleteMachine}>Delete</button>
+    </div>
 
-    <label for="name">Name</label>
-    <input type="text" id="name" bind:value={$machine.name} placeholder="Name" disabled={isViewing} />
+    <form>
+        <label for="id">ID</label>
+        <input type="text" id="id" bind:value={$machine.id} disabled />
 
-    <label for="make">Make</label>
-    <input type="text" id="make" bind:value={$machine.make} placeholder="Make" disabled={isViewing} />
+        <label for="name">Name</label>
+        <input type="text" id="name" bind:value={$machine.name} placeholder="Name" disabled={isViewing} />
 
-    <label for="type">Type</label>
-    <select id="type" value="hi" disabled={isViewing}>
-        {#each $machineTypes as machineType}
-            <option value={machineType.id}>{machineType.name}</option>
-        {/each}
-    </select>
+        <label for="make">Make</label>
+        <input type="text" id="make" bind:value={$machine.make} placeholder="Make" disabled={isViewing} />
 
-    <label for="status">Status</label>
-    <select id="status" disabled={isViewing}>
-        {#each $machineStatuses as machineStatus}
-            <option value={machineStatus.id}>{machineStatus.name}</option>
-        {/each}
-    </select>
+        <label for="type">Type</label>
+        <select id="type" value="hi" disabled={isViewing}>
+            {#each $machineTypes as machineType}
+                <option value={machineType.id}>{machineType.name}</option>
+            {/each}
+        </select>
 
-    <label for="facility">Facility</label>
-    <select id="facility" disabled={isViewing}>
-        {#each $facilities as facility}
-            <option value={facility.id}>{facility.name}</option>
-        {/each}
-    </select>
-</form>
+        <label for="status">Status</label>
+        <select id="status" disabled={isViewing}>
+            {#each $machineStatuses as machineStatus}
+                <option value={machineStatus.id}>{machineStatus.name}</option>
+            {/each}
+        </select>
+
+        <label for="facility">Facility</label>
+        <select id="facility" disabled={isViewing}>
+            {#each $facilities as facility}
+                <option value={facility.id}>{facility.name}</option>
+            {/each}
+        </select>
+    </form>
+</tab>
+
+<tab> </tab>
+
+<tab> </tab>
+
+<style>
+</style>
