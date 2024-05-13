f25686ed5b0a92abfe7283244b3a8bf3fcea98d9
Henning Cullin <henningcullin@gmail.com>
2024-05-13 09:46:47 +0200

    feat: basic state machine

--- frontend/src/routes/machines/Machine.svelte
+++ frontend/src/routes/machines/Machine.svelte
@@ -9,6 +9,14 @@
     getMachineTypes();
     getMachineStatuses();
     getFacilities();
+
+    const url = new URL(location.href).searchParams;
+    const params = url.searchParams;
+
+    $: isCreating = new URL(location?.href)?.searchParams?.get('new');
+    $: isEditing = new URL(location?.href)?.searchParams?.get('edit');
+
+    $: isViewing = !(isCreating || isEditing);
 </script>
 
 <form>
@@ -16,27 +24,27 @@
     <input type="text" id="id" bind:value={$machine.id} disabled />
 
     <label for="name">Name</label>
-    <input type="text" id="name" bind:value={$machine.name} disabled />
+    <input type="text" id="name" bind:value={$machine.name} placeholder="Name" disabled={isViewing} />
 
     <label for="make">Make</label>
-    <input type="text" id="make" bind:value={$machine.make} disabled />
+    <input type="text" id="make" bind:value={$machine.make} placeholder="Make" disabled={isViewing} />
 
     <label for="type">Type</label>
-    <select id="type" value="hi" disabled>
+    <select id="type" value="hi" disabled={isViewing}>
         {#each $machineTypes as machineType}
             <option value={machineType.id}>{machineType.name}</option>
         {/each}
     </select>
 
     <label for="status">Status</label>
-    <select id="status" disabled>
+    <select id="status" disabled={isViewing}>
         {#each $machineStatuses as machineStatus}
             <option value={machineStatus.id}>{machineStatus.name}</option>
         {/each}
     </select>
 
     <label for="facility">Facility</label>
-    <select id="facility" disabled>
+    <select id="facility" disabled={isViewing}>
         {#each $facilities as facility}
             <option value={facility.id}>{facility.name}</option>
         {/each}
