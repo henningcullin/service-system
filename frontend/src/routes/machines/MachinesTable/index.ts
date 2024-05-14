040af9e141c8bc40f3653e43ab935751cd6d72c1
Henning Cullin <henningcullin@gmail.com>
2024-05-14 20:45:26 +0200

    refactor: removed priority component

--- frontend/src/routes/machines/MachinesTable/index.ts
+++ frontend/src/routes/machines/MachinesTable/index.ts
@@ -5,7 +5,6 @@ export { default as DataTableMachineStatusCell } from "./data-table-machine-stat
 export { default as DataTableFacilityCell } from "./data-table-facility-cell.svelte";
 export { default as DataTableDateCell } from "./data-table-date-cell.svelte";
 export { default as DataTableRowActions } from "./data-table-row-actions.svelte";
-export { default as DataTablePriorityCell } from "./data-table-priority-cell.svelte";
 export { default as DataTableColumnHeader } from "./data-table-column-header.svelte";
 export { default as DataTableToolbar } from "./data-table-toolbar.svelte";
 export { default as DataTablePagination } from "./data-table-pagination.svelte";
