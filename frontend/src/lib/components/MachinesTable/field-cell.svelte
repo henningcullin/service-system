901ae597a3e55e4309efd10f88f13e3991b599f3
Henning Cullin <henningcullin@gmail.com>
2024-05-14 22:57:16 +0200

    fix: only display active

--- frontend/src/lib/components/MachinesTable/field-cell.svelte
+++ frontend/src/lib/components/MachinesTable/field-cell.svelte
@@ -2,8 +2,10 @@
     export let value: string;
 </script>
 
-<div class="flex space-x-2">
-    <span class="max-w-[250px] truncate font-medium">
-        {value}
-    </span>
-</div>
+{#if value}
+    <div class="flex space-x-2">
+        <span class="max-w-[250px] truncate font-medium">
+            {value}
+        </span>
+    </div>
+{/if}
