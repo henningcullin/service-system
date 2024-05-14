6e90cd47f871725b2c8c714691845a33f19cc822
Henning Cullin <henningcullin@gmail.com>
2024-05-14 20:17:38 +0200

    feat: use new sorting stuff

--- frontend/src/routes/machines/MachinesTable/data-table-toolbar.svelte
+++ frontend/src/routes/machines/MachinesTable/data-table-toolbar.svelte
@@ -2,27 +2,30 @@
     import type { TableViewModel } from 'svelte-headless-table';
     import X from 'lucide-svelte/icons/x';
     import type { Writable } from 'svelte/store';
-    import { priorities, statuses } from './data';
-    import type { Task } from './schema';
+    import { machineTypes, machineStatuses, facilities } from '$stores'; // import from $stores
+    import type { Machine } from './schema';
     import { DataTableFacetedFilter, DataTableViewOptions } from './index.js';
     import Button from '$lib/components/ui/button/button.svelte';
     import { Input } from '$lib/components/ui/input/index.js';
 
-    export let tableModel: TableViewModel<Task>;
-    export let data: Task[];
+    export let tableModel: TableViewModel<Machine>;
+    export let data: Machine[];
 
     const counts = data.reduce<{
+        machine_type: { [index: string]: number };
         status: { [index: string]: number };
-        priority: { [index: string]: number };
+        facility: { [index: string]: number };
     }>(
-        (acc, { status, priority }) => {
-            acc.status[status] = (acc.status[status] || 0) + 1;
-            acc.priority[priority] = (acc.priority[priority] || 0) + 1;
+        (acc, { machine_type, status, facility }) => {
+            acc.machine_type[machine_type.name] = (acc.machine_type[machine_type.name] || 0) + 1;
+            acc.status[status.name] = (acc.status[status.name] || 0) + 1;
+            acc.facility[facility.name] = (acc.facility[facility.name] || 0) + 1;
             return acc;
         },
         {
+            machine_type: {},
             status: {},
-            priority: {},
+            facility: {},
         },
     );
 
@@ -37,8 +40,9 @@
         filterValues,
     }: {
         filterValues: Writable<{
+            machine_type: string[];
             status: string[];
-            priority: string[];
+            facility: string[];
         }>;
     } = pluginStates.colFilter;
 
@@ -55,23 +59,30 @@
         />
 
         <DataTableFacetedFilter
+            bind:filterValues={$filterValues.machine_type}
+            title="Type"
+            options={$machineTypes}
+            counts={counts.machine_type}
+        />
+        <DataTableFacetedFilter
             bind:filterValues={$filterValues.status}
             title="Status"
-            options={statuses}
+            options={$machineStatuses}
             counts={counts.status}
         />
         <DataTableFacetedFilter
-            bind:filterValues={$filterValues.priority}
-            title="Priority"
-            options={priorities}
-            counts={counts.priority}
+            bind:filterValues={$filterValues.facility}
+            title="Facilites"
+            options={$facilities}
+            counts={counts.facility}
         />
         {#if showReset}
             <Button
                 on:click={() => {
                     $filterValue = '';
+                    $filterValues.machine_type = [];
                     $filterValues.status = [];
-                    $filterValues.priority = [];
+                    $filterValues.facility = [];
                 }}
                 variant="ghost"
                 class="h-8 px-2 lg:px-3"
