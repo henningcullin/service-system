b37d1fe004ebcb2a6150db59a375425cac6d3a22
Henning Cullin <henningcullin@gmail.com>
2024-05-14 23:18:59 +0200

    feat: delete button with alert

--- frontend/src/lib/components/MachinesTable/row-actions.svelte
+++ frontend/src/lib/components/MachinesTable/row-actions.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import Ellipsis from 'lucide-svelte/icons/ellipsis';
     import { type Machine, machineSchema } from './schema';
-    import { Button } from '$lib/components/ui/button/index.js';
-    import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+    import { Button } from '$components/ui/button/index.js';
+    import * as DropdownMenu from '$components/ui/dropdown-menu/index.js';
+    import * as AlertDialog from '$components/ui/alert-dialog/index.js';
+
+    async function deleteMachine() {
+        console.log(machine);
+    }
+
+    let deleteDialogOpen = false;
 
     export let row: Machine;
-    const task = machineSchema.parse(row);
+    const machine = machineSchema.parse(row);
 </script>
 
 <DropdownMenu.Root>
@@ -17,12 +24,23 @@
     </DropdownMenu.Trigger>
     <DropdownMenu.Content class="w-[160px]" align="end">
         <DropdownMenu.Item>Edit</DropdownMenu.Item>
-        <DropdownMenu.Item>Make a copy</DropdownMenu.Item>
         <DropdownMenu.Item>Favorite</DropdownMenu.Item>
         <DropdownMenu.Separator />
-        <DropdownMenu.Item>
-            Delete
-            <DropdownMenu.Shortcut>⌘⌫</DropdownMenu.Shortcut>
-        </DropdownMenu.Item>
+        <DropdownMenu.Item on:click={() => (deleteDialogOpen = true)}>Delete</DropdownMenu.Item>
     </DropdownMenu.Content>
 </DropdownMenu.Root>
+
+<AlertDialog.Root bind:open={deleteDialogOpen}>
+    <AlertDialog.Content>
+        <AlertDialog.Header>
+            <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+            <AlertDialog.Description>
+                This action cannot be undone. This will permanently delete the machine from our servers.
+            </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+            <AlertDialog.Action on:click={deleteMachine}>Continue</AlertDialog.Action>
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
