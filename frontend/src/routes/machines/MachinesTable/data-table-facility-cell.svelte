9f1e715b20d07d2bc36d1143fee2d6f71b74f760
Henning Cullin <henningcullin@gmail.com>
2024-05-14 20:53:29 +0200

    feat: added display of facilities

--- frontend/src/routes/machines/MachinesTable/data-table-facility-cell.svelte
+++ frontend/src/routes/machines/MachinesTable/data-table-facility-cell.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
-    export let value: string;
+    import * as HoverCard from '$components/ui/hover-card';
+    import { Link } from 'svelte-navigator';
+
+    export let value: { id: string; name: string; address?: string };
 </script>
 
-<div class="flex space-x-2">
-    <span class="max-w-[200px] truncate font-medium">
-        {value}
-    </span>
-</div>
+{#if value.id}
+    <div class="flex space-x-2">
+        <span class="max-w-[200px] truncate font-medium">
+            <HoverCard.Root>
+                <HoverCard.Trigger
+                    class="rounded-sm underline-offset-4 hover:underline focus-visible:outline-2 focus-visible:outline-offset-8 focus-visible:outline-black"
+                >
+                    <Link to="/facility/{value.id}">{value.name}</Link>
+                </HoverCard.Trigger>
+                <HoverCard.Content class="w-80">
+                    <div class="flex justify-between space-x-4">
+                        <div class="space-y-1">
+                            <h4 class="text-sm font-semibold">{value.name}</h4>
+                            {#if value.address}
+                                <p class="text-sm">{value.address}</p>
+                            {/if}
+                            <div class="flex items-center pt-2">
+                                <span class="text-xs text-muted-foreground"> {value.id} </span>
+                            </div>
+                        </div>
+                    </div>
+                </HoverCard.Content>
+            </HoverCard.Root>
+        </span>
+    </div>
+{/if}
