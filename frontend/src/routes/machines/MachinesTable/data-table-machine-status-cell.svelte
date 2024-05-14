7f8fd32d1239465b6ea25a4603fbc0d9728f73ee
Henning Cullin <henningcullin@gmail.com>
2024-05-14 19:58:37 +0200

    feat: machineStatus cell

---
+++ frontend/src/routes/machines/MachinesTable/data-table-machine-status-cell.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { statuses } from './data';
+
+    export let value: string;
+    const status = statuses.find((status) => status.value === value);
+</script>
+
+{#if status}
+    <div class="flex w-[100px] items-center">
+        <span>{status.label}</span>
+    </div>
+{/if}
