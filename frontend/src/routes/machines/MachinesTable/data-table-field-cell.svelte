b8747458d4bde3deaf52b41dbf6beb608b047f9c
Henning Cullin <henningcullin@gmail.com>
2024-05-14 19:58:27 +0200

    feat: field cell

---
+++ frontend/src/routes/machines/MachinesTable/data-table-field-cell.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let value: string;
+</script>
+
+<div class="flex space-x-2">
+    <span class="max-w-[200px] truncate font-medium">
+        {value}
+    </span>
+</div>
