d5d23243faf5df42c402492ecf53158ccf941e62
Henning Cullin <henningcullin@gmail.com>
2024-05-14 19:57:58 +0200

    feat: machine type and zod schema

--- frontend/src/routes/machines/MachinesTable/schema.ts
+++ frontend/src/routes/machines/MachinesTable/schema.ts
@@ -2,12 +2,27 @@ import { z } from "zod";
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
-export const taskSchema = z.object({
-	id: z.string(),
-	title: z.string(),
-	status: z.string(),
-	label: z.string(),
-	priority: z.string(),
+
+export const machineSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    make: z.string().nullable(),
+    machine_type: z.object({
+        id: z.string(),
+        name: z.string(),
+    }),
+    status: z.object({
+        id: z.string(),
+        name: z.string(),
+    }),
+    created: z.string(),
+    edited: z.string(),
+    facility: z.object({
+        id: z.string().nullable(),
+        name: z.string().nullable(),
+        address: z.string().nullable(),
+    }),
+    image: z.string().nullable(),
 });
 
-export type Task = z.infer<typeof taskSchema>;
+export type Machine = z.infer<typeof machineSchema>;
