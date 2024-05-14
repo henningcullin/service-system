7259a9e4c14d3d9970f8c742e59fa8e92b127555
Henning Cullin <henningcullin@gmail.com>
2024-05-14 20:59:37 +0200

    fix: retrieve facilities

--- frontend/src/routes/machines/NewMachines.svelte
+++ frontend/src/routes/machines/NewMachines.svelte
@@ -1,10 +1,11 @@
 <script>
     import MachineTable from './MachinesTable/data-table.svelte';
-    import { getMachineStatuses, getMachineTypes, getMachines } from '$utils';
+    import { getMachineStatuses, getMachineTypes, getMachines, getFacilities } from '$utils';
 
     getMachineStatuses();
     getMachineTypes();
     getMachines();
+    getFacilities();
 </script>
 
 <div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
