ece03470d4d84210477622ca2fbd9e1df2ace67d
Henning Cullin <henningcullin@gmail.com>
2024-05-14 20:17:18 +0200

    refactor: remove unnecessary

--- frontend/src/routes/machines/NewMachines.svelte
+++ frontend/src/routes/machines/NewMachines.svelte
@@ -1,6 +1,5 @@
 <script>
     import MachineTable from './MachinesTable/data-table.svelte';
-    import { machineStatuses, machineTypes, machines } from '$stores';
     import { getMachineStatuses, getMachineTypes, getMachines } from '$utils';
 
     getMachineStatuses();
@@ -9,5 +8,5 @@
 </script>
 
 <div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-    <MachineTable data={$machines} />
+    <MachineTable />
 </div>
