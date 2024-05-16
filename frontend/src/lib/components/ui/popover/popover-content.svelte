aee6b6615af0e173b62c0734929a1721bebf4dc8
Henning Cullin <henningcullin@gmail.com>
2024-05-16 22:23:44 +0200

    refactor: newyork popover

--- frontend/src/lib/components/ui/popover/popover-content.svelte
+++ frontend/src/lib/components/ui/popover/popover-content.svelte
@@ -4,17 +4,21 @@
 	let className = undefined;
 	export let transition = flyAndScale;
 	export let transitionConfig = undefined;
+	export let align = "center";
+	export let sideOffset = 4;
 	export { className as class };
 </script>
 
 <PopoverPrimitive.Content
 	{transition}
 	{transitionConfig}
+	{align}
+	{sideOffset}
+	{...$$restProps}
 	class={cn(
 		"z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
 		className
 	)}
-	{...$$restProps}
 >
 	<slot />
 </PopoverPrimitive.Content>
