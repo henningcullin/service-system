c1a947edb99bb084601d266e3d436f0cd278cfb7
Henning Cullin <henningcullin@gmail.com>
2024-05-16 18:30:41 +0200

    refactor: componentized select fields

---
+++ frontend/src/routes/machines/Machine/Select.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Label from '$components/ui/label/label.svelte';
+    import * as Select from '$components/ui/select/index.js';
+    import { Writable } from 'svelte/store';
+    import { isViewing } from './common';
+
+    export let properties: { id: string; label: string; placeholder: string };
+    export let selected: { label: string; value: string };
+    export let onSelectedChange;
+    export let errors: string = '';
+</script>
+
+<div>
+    <Label for={properties.id} class={errors ? 'text-red-800' : ''}>{properties.label}</Label>
+    <Select.Root disabled={$isViewing} bind:selected {onSelectedChange}>
+        <Select.Trigger>
+            <Select.Value placeholder={properties.placeholder} />
+        </Select.Trigger>
+        <Select.Content>
+            <slot />
+        </Select.Content>
+    </Select.Root>
+    {#if errors}
+        <p class="text-red-800 ml-auto text-xs pt-1">{errors}</p>
+    {/if}
+</div>
