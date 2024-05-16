598fce0d78bf3176df635cc8f2d25c2c51de03f9
Henning Cullin <henningcullin@gmail.com>
2024-05-16 21:38:28 +0200

    refactor: newyork hovercard

--- frontend/src/lib/components/ui/hover-card/hover-card-content.svelte
+++ frontend/src/lib/components/ui/hover-card/hover-card-content.svelte
@@ -2,20 +2,20 @@
 	import { LinkPreview as HoverCardPrimitive } from "bits-ui";
 	import { cn, flyAndScale } from "/utils.js";
 	let className = undefined;
-	export let transition = flyAndScale;
-	export let transitionConfig = undefined;
 	export let align = "center";
 	export let sideOffset = 4;
+	export let transition = flyAndScale;
+	export let transitionConfig = undefined;
 	export { className as class };
 </script>
 
 <HoverCardPrimitive.Content
 	{transition}
 	{transitionConfig}
-	{align}
 	{sideOffset}
+	{align}
 	class={cn(
-		"z-50 mt-3 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
+		"z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
 		className
 	)}
 	{...$$restProps}
