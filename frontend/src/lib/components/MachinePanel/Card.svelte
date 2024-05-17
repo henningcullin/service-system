c3054dcd0653a506dfbb28b59cce4d8be6d888bc
Henning Cullin <henningcullin@gmail.com>
2024-05-17 14:25:34 +0200

    feat: added more stuff

--- frontend/src/lib/components/MachinePanel/Card.svelte
+++ frontend/src/lib/components/MachinePanel/Card.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
+    import * as AlertDialog from '$components/ui/alert-dialog/index.js';
     import * as Card from '$components/ui/card/index.js';
     import * as Select from '$components/ui/select/index.js';
     import Button from '$components/ui/button/button.svelte';
     import Input from '$components/ui/input/input.svelte';
     import Label from '$components/ui/label/label.svelte';
+    import { sendDelete } from '$utils';
 
-    export let props: { title: string; desc: string; selectPlaceholder: string; namePlaceholder: string };
+    const VIEWING_STATE = 0;
+    const CREATING_STATE = 1;
+    const EDITING_STATE = 2;
+
+    let open = false;
+    let state = '';
+
+    $: selectedValue = {};
+
+    function onSelectedChange() {}
+
+    async function deleteItem() {
+        sendDelete(`/api/auth/${apiEndpoint}/?id=${$formStore.id}`);
+    }
+
+    function createItem() {}
+
+    function updateItem() {}
+
+    export let formStore;
+    export let apiEndpoint: string;
+    export let cardProps: { title: string; desc: string };
+    export let formProps: { selectPlaceholder: string; namePlaceholder: string };
 </script>
 
 <Card.Root>
     <Card.Header>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Description>{props.desc}</Card.Description>
+        <Card.Title>{cardProps.title}</Card.Title>
+        <Card.Description>{cardProps.desc}</Card.Description>
     </Card.Header>
     <Card.Content>
         <form>
             <div class="grid w-full items-center gap-4">
-                <Select.Root>
+                <Select.Root {onSelectedChange}>
                     <Select.Trigger>
-                        <Select.Value placeholder={props.selectPlaceholder} />
+                        <Select.Value placeholder={formProps.selectPlaceholder} />
                     </Select.Trigger>
                     <Select.Content>
                         <slot />
@@ -26,7 +50,7 @@
                 </Select.Root>
                 <div class="flex flex-col space-y-1.5">
                     <Label for="name">Name</Label>
-                    <Input id="name" placeholder={props.namePlaceholder} />
+                    <Input id="name" placeholder={formProps.namePlaceholder} />
                 </div>
                 <div class="flex flex-col space-y-1.5">
                     <Label for="id">Id</Label>
@@ -36,8 +60,34 @@
         </form>
     </Card.Content>
     <Card.Footer class="flex justify-between">
-        <Button variant="outline">New</Button>
-        <Button variant="destructive">Delete</Button>
-        <Button>Save</Button>
+        <Button
+            variant="outline"
+            on:click={() => {
+                formStore.set({ id: '', name: '' });
+            }}>New</Button
+        >
+        <Button
+            variant="destructive"
+            on:click={() => {
+                open = true;
+            }}>Delete</Button
+        >
+        <Button on:click={() => {}}>Save</Button>
     </Card.Footer>
 </Card.Root>
+
+<AlertDialog.Root bind:open>
+    <AlertDialog.Content>
+        <AlertDialog.Header>
+            <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+            <AlertDialog.Description>
+                This action cannot be undone. This will permanently delete the '{$formStore.name}' {cardProps.title} from
+                our servers.
+            </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+            <AlertDialog.Action on:click={deleteItem}>Continue</AlertDialog.Action>
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
