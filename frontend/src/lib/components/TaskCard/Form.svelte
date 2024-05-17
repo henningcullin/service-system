adbbc5fe5301f4e84a0de00e04a3d2a2e2fd2e0a
Henning Cullin <henningcullin@gmail.com>
2024-05-17 20:43:01 +0200

    refactor: bunch of stuff

--- frontend/src/lib/components/TaskCard/Form.svelte
+++ frontend/src/lib/components/TaskCard/Form.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { writable } from 'svelte/store';
-    import { machine, machineTypes, machineStatuses, facilities } from '$stores';
+    import { task, taskTypes, taskStatuses, users, machines } from '$stores';
     import { sendJSON } from '$utils';
     import { navigate } from 'svelte-navigator';
     import Input from './Input.svelte';
@@ -19,32 +19,42 @@
     import { z } from 'zod';
     import Select from './Select.svelte';
     import SelectItem from '$components/ui/select/select-item.svelte';
+    import { toast } from 'svelte-sonner';
 
     const selectedType = writable({ label: '', value: '' });
     const selectedStatus = writable({ label: '', value: '' });
-    const selectedFacility = writable({ label: '', value: '' });
+    /* const selectedFacility = writable({ label: '', value: '' }); */
 
     $: selectedType.set(
-        $form.machine_type
-            ? { label: $machineTypes?.find((mt) => mt.id === $form?.machine_type)?.name, value: $form?.machine_type }
+        $form.task_type
+            ? { label: $taskTypes?.find((mt) => mt.id === $form?.task_type)?.name, value: $form?.task_type }
             : null,
     );
     $: selectedStatus.set(
         $form.status
-            ? { label: $machineStatuses?.find((ms) => ms.id === $form?.status)?.name, value: $form?.status }
+            ? { label: $taskStatuses?.find((ms) => ms.id === $form?.status)?.name, value: $form?.status }
             : null,
     );
-    $: selectedFacility.set(
+    /*     $: selectedFacility.set(
         $form.facility
             ? { label: $facilities?.find((f) => f.id === $form?.facility)?.name, value: $form?.facility }
             : null,
-    );
+    ); */
 
     $: {
         if (!$isViewing) {
             try {
                 formSchema.parse($form);
-                $fieldErrors = { name: '', make: '', machine_type: '', status: '', facility: '' };
+                $fieldErrors = {
+                    title: '',
+                    description: '',
+                    task_type: '',
+                    status: '',
+                    archived: '',
+                    executors: '',
+                    machine: '',
+                    due_at: '',
+                };
                 $hasErrors = false;
             } catch (e) {
                 if (e instanceof z.ZodError) {
@@ -54,88 +64,96 @@
                 }
             }
         } else {
-            $fieldErrors = { name: '', make: '', machine_type: '', status: '', facility: '' };
+            $fieldErrors = {
+                title: '',
+                description: '',
+                task_type: '',
+                status: '',
+                archived: '',
+                executors: '',
+                machine: '',
+                due_at: '',
+            };
             $hasErrors = false;
         }
     }
 
     let isSaving = false;
 
-    async function saveMachine() {
+    async function saveTask() {
         if (isSaving) return;
         isSaving = true;
         switch (true) {
             case $isViewing:
                 return (isSaving = false);
             case $isCreating:
-                await createMachine();
+                await createTask();
                 break;
             case $isEditing:
-                await updateMachine();
+                await updateTask();
                 break;
         }
         isSaving = false;
     }
 
-    async function createMachine() {
+    async function createTask() {
         try {
-            const { name, make, machine_type, status, facility } = $form;
+            const { title, description, task_type, status } = $form;
             const response = await sendJSON('/api/auth/machine', 'POST', {
-                name,
-                make,
-                machine_type,
+                title,
+                description,
+                task_type,
                 status,
-                facility,
             });
-            if (response.status !== 201) return alert('Failed to create the machine');
+            if (response.status !== 201) return toast.error('Failed to create the task');
             const data = await response.json();
-            machine.set(data);
-            updateUrl($machine.id);
+            task.set(data);
+            updateUrl($task.id);
             navigate('?view=true');
             loadFields();
         } catch (error) {
-            console.error(error);
+            toast.error('Failed to create the task');
         }
     }
 
-    async function updateMachine() {
+    async function updateTask() {
         try {
             const changedFields = { id: $form.id };
             for (const field in $form) {
-                if ($form[field] !== $machine[field]) {
+                if ($form[field] !== $task[field] && field !== 'executors') {
                     changedFields[field] = $form[field];
                 }
             }
             if (Object.keys(changedFields).length < 2) return;
             const response = await sendJSON('/api/auth/machine', 'PUT', changedFields);
-            if (response.status !== 200) return alert('Failed to update the machine');
+            if (response.status !== 200) return toast.error('Failed to update the task');
             const data = await response.json();
-            machine.set(data);
+            task.set(data);
             navigate('?view=true');
             loadFields();
         } catch (error) {
-            console.error(error);
+            toast.error('Failed to update the task');
         }
     }
 </script>
 
-<form on:submit|preventDefault={saveMachine} class="space-y-4 w-full md:w-auto">
+<form on:submit|preventDefault={saveTask} class="space-y-4 w-full md:w-auto">
     <Input properties={{ id: 'id', label: 'Id' }} bind:value={$form.id} disabled={true} />
 
-    <Input properties={{ id: 'name', label: 'Name' }} bind:value={$form.name} errors={$fieldErrors.name} />
+    <Input properties={{ id: 'title', label: 'Title' }} bind:value={$form.title} errors={$fieldErrors.title} />
 
-    <Input properties={{ id: 'make', label: 'Make' }} bind:value={$form.make} />
+    <Input properties={{ id: 'description', label: 'Description' }} bind:value={$form.description} />
 
     <Select
         properties={{ id: 'machine_type', label: 'Type', placeholder: 'Select a type' }}
         bind:selected={$selectedType}
         onSelectedChange={(opt) => {
-            opt && ($form.machine_type = opt.value);
+            opt && ($form.task_type = opt.value);
         }}
-        errors={$fieldErrors?.machine_type}
+        errors={$fieldErrors?.task_type}
     >
-        {#each $machineTypes as machineType}
-            <SelectItem value={machineType.id} label={machineType.name} />
+        {#each $taskTypes as taskType}
+            <SelectItem value={taskType.id} label={taskType.name} />
         {/each}
     </Select>
 
@@ -147,12 +165,12 @@
         }}
         errors={$fieldErrors?.status}
     >
-        {#each $machineStatuses as machineStatus}
-            <SelectItem value={machineStatus.id} label={machineStatus.name} />
+        {#each $taskStatuses as taskStatus}
+            <SelectItem value={taskStatus.id} label={taskStatus.name} />
         {/each}
     </Select>
 
-    <Select
+    <!--     <Select
         properties={{ id: 'facility', label: 'Facility', placeholder: 'Select a facility' }}
         bind:selected={$selectedFacility}
         onSelectedChange={(opt) => {
@@ -163,7 +181,7 @@
         {#each $facilities as facility}
             <SelectItem value={facility.id} label={facility.name} />
         {/each}
-    </Select>
+    </Select> -->
 
     <div>
         <div class="ml-auto text-xs text-muted-foreground">Created {$form.created}</div>
