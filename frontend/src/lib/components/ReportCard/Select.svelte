bea4de47b7adfc416426287c9f223622d75988d7
Henning Cullin <henningcullin@gmail.com>
2024-05-18 17:37:40 +0200

    fix: remove shit

--- frontend/src/lib/components/ReportCard/Select.svelte
+++ frontend/src/lib/components/ReportCard/Select.svelte
@@ -5,14 +5,13 @@
 
     export let properties: { id: string; label: string; placeholder: string };
     export let selected;
-    export let multiple = false;
     export let onSelectedChange;
     export let errors: string = '';
 </script>
 
 <div>
     <Label for={properties.id} class={errors ? 'text-red-800' : ''}>{properties.label}</Label>
-    <Select.Root disabled={$isViewing} {multiple} bind:selected {onSelectedChange}>
+    <Select.Root disabled={$isViewing} bind:selected {onSelectedChange}>
         <Select.Trigger>
             <Select.Value placeholder={properties.placeholder} />
         </Select.Trigger>
