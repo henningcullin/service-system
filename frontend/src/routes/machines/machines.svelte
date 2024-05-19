6b413aadd4f4ff2698b734871cda4bd7baac51fd
Henning Cullin <henningcullin@gmail.com>
2024-05-19 22:09:14 +0200

    feat: added cards for all

--- frontend/src/routes/machines/machines.svelte
+++ frontend/src/routes/machines/machines.svelte
@@ -2,6 +2,10 @@
     import MachineTable from '$components/MachinesTable/table.svelte';
     import { getMachineStatuses, getMachineTypes, getMachines, getFacilities } from '$utils';
 
+    import { machines } from '$stores';
+    import * as Card from '$components/ui/card';
+    import { Link } from 'svelte-navigator';
+
     getMachineStatuses();
     getMachineTypes();
     getMachines();
@@ -15,4 +19,21 @@
         </div>
     </div>
     <MachineTable />
+</div>
+
+<div class="h-full flex-1 flex-col space-y-8 p-4 md:hidden">
+    <div class="flex items-center justify-between space-y-2">
+        <div>
+            <h2 class="text-2xl font-bold tracking-tight">Machines</h2>
+        </div>
+    </div>
+    {#each $machines as machine}
+        <Card.Root class="w-[100%]">
+            <Card.Header>
+                <Card.Title><Link to="/machine/{machine?.id}">{machine?.name}</Link></Card.Title>
+                <Card.Description></Card.Description>
+                <Card.Content></Card.Content>
+            </Card.Header>
+        </Card.Root>
+    {/each}
 </div>
--- frontend/src/routes/reports/reports.svelte
+++ frontend/src/routes/reports/reports.svelte
@@ -1,6 +1,9 @@
 <script>
     import ReportTable from '$components/ReportsTable/table.svelte';
+    import { reports } from '$stores';
     import { getReports } from '$utils';
+    import * as Card from '$components/ui/card';
+    import { Link } from 'svelte-navigator';
     getReports();
 </script>
 
@@ -11,4 +14,20 @@
         </div>
     </div>
     <ReportTable />
+</div>
+
+<div class="h-full flex-1 flex-col space-y-8 p-4 md:hidden">
+    <div class="flex items-center justify-between space-y-2">
+        <div>
+            <h2 class="text-2xl font-bold tracking-tight">Reports</h2>
+        </div>
+    </div>
+    {#each $reports as report}
+        <Card.Root class="w-[100%]">
+            <Card.Header>
+                <Card.Title><Link to="/report/{report?.id}">{report?.title}</Link></Card.Title>
+                <Card.Description>{report?.description}</Card.Description>
+            </Card.Header>
+        </Card.Root>
+    {/each}
 </div>
