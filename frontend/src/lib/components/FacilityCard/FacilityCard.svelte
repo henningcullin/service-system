4b1d83cd42d53973ebc0dab95d8cc7531f2c1b98
Henning Cullin <henningcullin@gmail.com>
2024-05-19 17:23:52 +0200

    feat: added select

--- frontend/src/lib/components/FacilityCard/FacilityCard.svelte
+++ frontend/src/lib/components/FacilityCard/FacilityCard.svelte
@@ -1,10 +1,22 @@
-<script lang="ts">
+<script>
     import Actions from './Actions.svelte';
     import { useLocation } from 'svelte-navigator';
-    import { onMount } from 'svelte';
     import Separator from '$components/ui/separator/separator.svelte';
-    import { id, isCreating, isEditing, isViewing } from './common';
+    import { id, isCreating, isEditing, isViewing, loadFields } from './common';
     import Form from './Form.svelte';
+    import { getFacilities } from '$utils';
+    import { onMount } from 'svelte';
+    import { facilities, facility } from '$stores';
+    import * as Select from '$components/ui/select/index.js';
+    import Label from '$components/ui/label/label.svelte';
+
+    $: selected = { value: $facility?.id, label: $facility?.name };
+
+    function onSelectedChange(opt) {
+        if (!opt) return;
+        facility.set($facilities.find((f) => f.id === opt?.value));
+        loadFields();
+    }
 
     const location = useLocation();
     $: params = new URLSearchParams($location.search);
@@ -16,12 +28,30 @@
     $: isEditing.set(params?.get('edit') === 'true' && !!$id);
     $: isViewing.set(!($isCreating || $isEditing));
 
-    onMount(async () => {});
+    onMount(async () => {
+        await getFacilities();
+        if ($id) {
+            const selected = $facilities.find((f) => f.id === $id);
+            facility.set({ ...selected });
+            loadFields();
+        }
+    });
 </script>
 
 <div class="space-y-0.5">
     <h2 class="text-2xl font-bold tracking-tight pb-2">Facility</h2>
     <Actions />
+    <Label>Selected facility</Label>
+    <Select.Root {selected} {onSelectedChange} class="mt-2">
+        <Select.Trigger>
+            <Select.Value placeholder="Select a facility" />
+        </Select.Trigger>
+        <Select.Content>
+            {#each $facilities as facility}
+                <Select.Item value={facility.id} label={facility.name} />
+            {/each}
+        </Select.Content>
+    </Select.Root>
 </div>
 <Separator class="my-6" />
 <div>
