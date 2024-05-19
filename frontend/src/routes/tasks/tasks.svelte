51bf725ec78fb83f59610e7e8726136ffa842796
Henning Cullin <henningcullin@gmail.com>
2024-05-19 21:21:52 +0200

    feat: cards for users and tasks

--- frontend/src/routes/tasks/tasks.svelte
+++ frontend/src/routes/tasks/tasks.svelte
@@ -1,6 +1,9 @@
 <script>
     import TaskTable from '$components/TasksTable/table.svelte';
+    import * as Card from '$components/ui/card';
+    import { tasks } from '$stores';
     import { getTasks } from '$utils';
+    import { Link } from 'svelte-navigator';
     getTasks();
 </script>
 
@@ -11,4 +14,20 @@
         </div>
     </div>
     <TaskTable />
+</div>
+
+<div class="h-full flex-1 flex-col space-y-8 p-4 md:hidden">
+    <div class="flex items-center justify-between space-y-2">
+        <div>
+            <h2 class="text-2xl font-bold tracking-tight">Tasks</h2>
+        </div>
+    </div>
+    {#each $tasks as task}
+        <Card.Root class="w-[100%]">
+            <Card.Header>
+                <Card.Title><Link to="/task/{task?.id}">{task?.title}</Link></Card.Title>
+                <Card.Description>{task?.description}</Card.Description>
+            </Card.Header>
+        </Card.Root>
+    {/each}
 </div>
--- frontend/src/routes/users/users.svelte
+++ frontend/src/routes/users/users.svelte
@@ -1,6 +1,9 @@
 <script>
     import UserTable from '$components/UsersTable/table.svelte';
+    import { users } from '$stores';
+    import * as Card from '$components/ui/card';
     import { getUsers } from '$utils';
+    import { Link } from 'svelte-navigator';
     getUsers();
 </script>
 
@@ -11,4 +14,20 @@
         </div>
     </div>
     <UserTable />
+</div>
+
+<div class="h-full flex-1 flex-col space-y-8 p-4 md:hidden">
+    <div class="flex items-center justify-between space-y-2">
+        <div>
+            <h2 class="text-2xl font-bold tracking-tight">Users</h2>
+        </div>
+    </div>
+    {#each $users as user}
+        <Card.Root class="w-[100%]">
+            <Card.Header>
+                <Card.Title><Link to="/user/{user?.id}">{user?.first_name} {user?.last_name}</Link></Card.Title>
+                <Card.Description><a href="mailto:{user?.email}">{user?.email}</a></Card.Description>
+            </Card.Header>
+        </Card.Root>
+    {/each}
 </div>
