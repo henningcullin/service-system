b37a3f63384f66858874749aba025b171f99b089
Henning Cullin <henningcullin@gmail.com>
2024-05-20 18:53:10 +0200

    feat: begun work on adding channels

--- frontend/src/routes/mainmenu/Mainmenu.svelte
+++ frontend/src/routes/mainmenu/Mainmenu.svelte
@@ -4,18 +4,21 @@
     import { Separator } from '$components/ui/separator';
     import * as Tabs from '$components/ui/tabs/';
     import { account, reports, tasks } from '$stores';
-    import * as Card from '$components/ui/card';
     import { getMyReports, getTasksToExecute } from '$utils';
     import { onMount } from 'svelte';
+    import TaskCard from '$components/MainMenu/TaskCard.svelte';
+    import ReportCard from '$components/MainMenu/ReportCard.svelte';
     import { Button } from '$components/ui/button/';
-    import { Link } from 'svelte-navigator';
 
-    onMount(async function () {
+    onMount(function () {
         reports.set([]);
         tasks.set([]);
         getMyReports($account?.id);
         getTasksToExecute($account?.id);
     });
+
+    const taskChannel = new EventSource('/api/auth/channels/tasks');
+    const reportChannel = new EventSource('/api/auth/channels/reports');
 
     let type = 'task';
     let activeTab = 'active';
@@ -23,13 +26,11 @@
 
 <div class="mainGrid">
     <div>
-        <Button on:click={() => (type = 'task')}>Tasks to do</Button>
-        <Button on:click={() => (type = 'report')}>My Reports</Button>
-    </div>
-    <div>
         <Tabs.Root bind:value={activeTab}>
             <div class="flex items-center px-4 py-2">
                 <h1 class="text-xl font-bold">Inbox</h1>
+                <Button on:click={() => (type = 'task')}>Tasks to do</Button>
+                <Button on:click={() => (type = 'report')}>My Reports</Button>
                 <Tabs.List class="ml-auto">
                     <Tabs.Trigger value="all" class="text-zinc-600 dark:text-zinc-200">All</Tabs.Trigger>
                     <Tabs.Trigger value="active" class="text-zinc-600 dark:text-zinc-200">Active</Tabs.Trigger>
@@ -44,66 +45,31 @@
                     </div>
                 </form>
             </div>
-            <Tabs.Content value="all" class="m-0">
-                {#if type === 'task'}
-                    {#each $tasks as task}
-                        <p>{task?.id}</p>
-                    {/each}
-                {:else}
-                    {#each $reports as report}
-                        <p>{report?.id}</p>{/each}
-                {/if}
-            </Tabs.Content>
-            <Tabs.Content value="active" class="m-0">
-                {#if type === 'task'}
-                    {#each $tasks?.filter((t) => !t?.archived) as task}
-                        <Card.Root>
-                            <Card.Header>
-                                <Card.Title
-                                    ><b class="ml-auto text-xs text-muted-foreground">Title </b><Link
-                                        to="/task/{task?.id}">{task?.title}</Link
-                                    ></Card.Title
-                                >
-                            </Card.Header>
-                            <Card.Content>
-                                <p>
-                                    <b class="ml-auto text-xs text-muted-foreground">Type</b>
-                                    <Link to="/task/panel/?type={task?.task_type?.id}">{task?.task_type?.name}</Link>
-                                </p>
-                                <p>
-                                    <b class="ml-auto text-xs text-muted-foreground">Status</b>
-                                    <Link to="/task/panel/?status={task?.status?.id}">{task?.status?.name}</Link>
-                                </p>
-                                {#if task?.machine?.id}
-                                    <p>
-                                        <b class="ml-auto text-xs text-muted-foreground">Machine</b>
-
-                                        <Link to="/machine/{task?.machine?.id}">{task?.machine?.name}</Link>
-                                    </p>
-                                {/if}
-                                <p>
-                                    <b class="ml-auto text-xs text-muted-foreground">Creator</b>
-                                    <Link to="/user/{task?.creator?.id}">
-                                        {task?.creator?.first_name}
-                                        {task?.creator?.last_name}
-                                    </Link>
-                                </p>
-                                <Separator class="my-1" />
-                                <h2 class="ml-auto text-xs text-muted-foreground">Description</h2>
-                                <span>
-                                    {task?.description}
-                                </span>
-                            </Card.Content>
-                        </Card.Root>
-                    {/each}
-                {:else}
-                    {#each $reports?.filter((r) => !r?.archived) as report}
-                        <p>{report?.id}</p>{/each}
-                {/if}
-            </Tabs.Content>
+            <div class="p-1">
+                <Tabs.Content value="all" class="p-2">
+                    {#if type === 'task'}
+                        {#each $tasks as task}
+                            <TaskCard {task} />
+                        {/each}
+                    {:else}
+                        {#each $reports as report}
+                            <ReportCard {report} />
+                        {/each}
+                    {/if}
+                </Tabs.Content>
+                <Tabs.Content value="active" class="p-2">
+                    {#if type === 'task'}
+                        {#each $tasks?.filter((t) => !t?.archived) as task}
+                            <TaskCard {task} />
+                        {/each}
+                    {:else}
+                        {#each $reports?.filter((r) => !r?.archived) as report}
+                            <ReportCard {report} />{/each}
+                    {/if}
+                </Tabs.Content>
+            </div>
         </Tabs.Root>
     </div>
-    <div>Panel 3</div>
 </div>
 
 <style>
@@ -112,11 +78,12 @@
         display: grid;
         grid-template-columns: 1fr 2fr 4fr;
         height: 90dvh;
-        width: 100%;
+        width: 90%;
     }
 
     .mainGrid > div {
         height: 100%;
+        width: 100%;
         border: 1px solid red;
     }
 </style>
